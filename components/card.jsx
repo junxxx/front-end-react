2840249d2cc0cf190c621b6599611025877d7d9a
junxxx <hprjunxxx@gmail.com>
2026-04-24 14:54:53 +0800

    add react jsx

---
+++ components/card.jsx
@@ -0,0 +1,7 @@
+export function Card() {
+  return (
+    <>
+      <p>This is a Card component</p>
+    </>
+  );
+}
--- index.jsx
+++ index.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import { localTest } from "./test.js";
 import localDefaultTest from "./test.js";
+import { Card } from "./components/card.jsx";
 
 const App = () => {
   localTest();
   localDefaultTest();
-  return React.createElement("h1", null, "Hello React");
+  React.createElement("h1", null, "Hello React");
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 };
 
 ReactDom.createRoot(document.getElementById("root")).render(

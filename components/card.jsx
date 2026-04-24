c51d40ec18f1e6f30a49193033e4bce7e9ce934c
junxxx <hprjunxxx@gmail.com>
2026-04-24 15:34:06 +0800

    displa pubic IP

--- components/card.jsx
+++ components/card.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 export function Card() {
+  function handleClick() {
+    console.log("clicked");
+  }
+  const [ip, setIp] = useState("");
+
+  useEffect(() => {
+    fetch("https://api64.ipify.org?format=json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Public IP:", data.ip);
+        setIp(data.ip);
+      })
+      .catch((error) => console.error("Error fetching IP:", error));
+  }, []);
+
   return (
     <>
-      <p>This is a Card component</p>
+      <div onClick={handleClick}>
+        <p>This is a Card component</p>
+        <p>Public IP: {ip}</p>
+      </div>
     </>
   );
 }

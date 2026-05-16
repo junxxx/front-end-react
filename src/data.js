ac891e13de8e20a246d39b810ef940297ae93977
junxxx <hprjunxxx@gmail.com>
2026-05-16 16:24:04 +0800

    update data

--- src/data.js
+++ src/data.js
@@ -1,4 +1,21 @@
 export const initialCommands = [
-  { id: 1, title: "NPM Proxy", command: "npm config set proxy http://localhost:7890", category: "Proxy" },
-  { id: 2, title: "Git Proxy", command: "git config --global http.proxy http://localhost:7890", category: "Proxy" },
+  {
+    id: 1,
+    title: "set http cli Proxy",
+    command:
+      "export http_proxy=http://127.0.0.1:8088;export https_proxy=http://127.0.0.1:8088;",
+    category: "Proxy",
+  },
+  {
+    id: 2,
+    title: "unset http cli Proxy",
+    command: "unset http_proxy;unset https_proxy",
+    category: "Proxy",
+  },
+  {
+    id: 3,
+    title: "npm registry",
+    command: "npm config set registry https://registry.npmmirror.com",
+    category: "Registry",
+  },
 ];
